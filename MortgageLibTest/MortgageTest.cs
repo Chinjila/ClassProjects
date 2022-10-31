35727e375a7f8d40fe60d2dc01e7103129d6414a
Nate Grabowski <nategrabowski@outlook.com>
2022-10-31 15:29:03 -0600

    added unit test

---
+++ MortgageLibTest/MortgageTest.cs
@@ -0,0 +1,28 @@
+﻿using MortagageLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageLibTest
+{
+    [TestClass]
+    public class MortgageTest
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void ConstructMortgageWithOriginationDateInThePastShouldThrowException() //next time use underscore to separate
+        {
+            //arrange
+            Mortgage m;
+            //act
+            m = new Mortgage(
+                DateTime.Now.AddDays(-3),
+                MortgageDuration.ThirtyYears
+                );
+            //assert
+            
+        }
+    }
+}
