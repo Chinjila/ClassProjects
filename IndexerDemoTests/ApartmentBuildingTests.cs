5d232d0f6842324e97cd303ae4a2a01440816198
victor <junk@objectmage.com>
2022-11-01 08:45:54 -0700

    Added IndexerDemoProject

---
+++ IndexerDemoTests/ApartmentBuildingTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IndexerDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerDemo.Tests
+{
+    [TestClass()]
+    public class ApartmentBuildingTests
+    {
+        [TestMethod()]
+        public void ApartmentBuildingTest()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            
+            //assert
+        }
+    }
+}
