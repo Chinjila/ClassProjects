c0665c5b64d62dddfc8a96f7359f1c40caf9ae8d
Nate Grabowski <nategrabowski@outlook.com>
2022-11-01 11:22:56 -0600

    ++2 additonal unit tests

--- IndexerDemo/ApartmentBuilding.cs
+++ IndexerDemo/ApartmentBuilding.cs
@@ -3,23 +3,41 @@
     public class ApartmentBuilding
     {
         Dictionary<string, Apartment> apartmentDirectory;
-            //List<Payment>
+        //List<Payment>
         public ApartmentBuilding()
         {
             apartmentDirectory = new Dictionary<string, Apartment>();
             for (int i = 0; i < 3; i++)
             {
-                apartmentDirectory.Add($"{i}A", new Apartment { Owner = $"Bob-{i}A"});
+                apartmentDirectory.Add($"{i}A", new Apartment { Owner = $"Bob-{i}A" });
                 apartmentDirectory.Add($"{i}B", new Apartment { Owner = $"Bob-{i}B" });
                 apartmentDirectory.Add($"{i}C", new Apartment { Owner = $"Bob-{i}C" });
             }
         }
-    }
 
-    public class Apartment {
-        public int Price { get; set; }
-        public string Owner { get; set; }
-        public int NumberOfRoom { get; set; }
-        public string AprtmentNumber { get; set; }
+        public int ApartmentCount
+        {
+            get
+
+            { return apartmentDirectory.Count; }
+        }
+
+        public Apartment GetApartment(string Apartment)
+        {
+            if (apartmentDirectory.TryGetValue(Apartment, out Apartment result))
+            { return result; }
+            else
+            {
+                throw new Exception("Apartment not found");
+            }
+        }
+
+        public class Apartment
+        {
+            public int Price { get; set; }
+            public string Owner { get; set; }
+            public int NumberOfRoom { get; set; }
+            public string ApartmentNumber { get; set; }
+        }
     }
 }
--- IndexerDemoTests/ApartmentBuildingTests.cs
+++ IndexerDemoTests/ApartmentBuildingTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static IndexerDemo.ApartmentBuilding;
 
 namespace IndexerDemo.Tests
 {
@@ -17,8 +18,42 @@ namespace IndexerDemo.Tests
             //arrange
             ApartmentBuilding b = new ApartmentBuilding();
             //act
-            
+            Assert.AreEqual(9, b.ApartmentCount);
+
             //assert
+        }
+        [TestMethod()]
+        public void Bob_1A_Lives_In_1A()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            Apartment a = b.GetApartment("1A");
+
+            //assert
+            Assert.AreEqual("Bob-1A", a.Owner);
+        }
+        [TestMethod()]
+        public void Bob_1A_Lives_In_1A_Fail()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            Apartment a = b.GetApartment("1C");
+
+            //assert
+            Assert.AreNotEqual("Bob-1A", a.Owner);
+        }
+        [TestMethod()]
+        public void Bob_Indexer_Test()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            Apartment a = b.GetApartment("1C");
+
+            //assert
+            Assert.AreNotEqual("Bob-1A", a.Owner);
         }
     }
 }
