fb78bc747af6dd2512c6ca284b28e5577363cfa6
victor <junk@objectmage.com>
2022-11-01 09:55:01 -0700

    Completed Indexer Sample

--- IndexerDemoTests/ApartmentBuildingTests.cs
+++ IndexerDemoTests/ApartmentBuildingTests.cs
@@ -17,8 +17,41 @@ namespace IndexerDemo.Tests
             //arrange
             ApartmentBuilding b = new ApartmentBuilding();
             //act
-            
+            Assert.AreEqual(9, b.ApartmentCount);
             //assert
+        }
+        [TestMethod()]
+        public void Bob_1A_lives_in_1A()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            Apartment a = b.GetApartment("1A");
+            //assert
+            Assert.AreEqual("Bob-1A", a.Owner);
+           
+        }
+        [TestMethod()]
+        public void When_Lookup_NonExisting_Apartment_Should_Throw_ANF_Exception()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+
+            //assert
+            Assert.ThrowsException<ApartmentNotFoundException>(() => b.GetApartment("5A"));
+
+        }
+
+        [TestMethod()]
+        public void ApartmentBuilding_Should_Support_Indexer()
+        {
+            //arrange
+            ApartmentBuilding b = new ApartmentBuilding();
+            //act
+            Apartment a = b["1A"];
+            //assert
+            Assert.AreEqual("Bob-1A", a.Owner);
         }
     }
 }
