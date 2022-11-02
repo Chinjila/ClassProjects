037a97d4d8a8befbc99787a879bb96fb2c4b17c7
Ty Marino <marinotyler@yahoo.com>
2022-11-02 13:59:45 -0700

    Better Timer added

---
+++ ConsoleApp1/BetterTimer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+
+    public delegate void TimerRing(BetterTimer timer, BetterTimerEventArgs arg);
+
+    public class BetterTimerEventArgs
+    {
+        public DateTime WhenStart;
+        public int HowManyTimesHasItfired;
+    }
+    public class BetterTimer
+    {
+        public bool runTimer = false;
+        DateTime whenStart;
+        int homManyTimesFired = 0;
+        public event TimerRing timerRing;
+        public BetterTimer(string name, int interval, bool start)
+        {
+            Name = name;
+            Interval = interval;
+            if (start)
+                runTimer = true;
+            Task t = new Task(async ()=> await EngageTimer());
+            t.Start();
+
+        }
+
+        private async Task EngageTimer()
+        {
+            whenStart = DateTime.Now;
+            while (runTimer)
+            {
+                await Task.Delay(Interval);   
+                BetterTimerEventArgs timerEventArgs = new BetterTimerEventArgs()
+                {
+                    HowManyTimesHasItfired = homManyTimesFired,
+                    WhenStart = whenStart
+                };
+            timerRing?.Invoke(this, timerEventArgs);
+                homManyTimesFired++;
+            }
+        }
+
+        public void Start()
+        {
+            if (!runTimer)
+            {
+                runTimer = true;
+                EngageTimer();
+            }
+
+        }
+        public void Stop()
+            {
+                if (runTimer)
+                    runTimer = false;
+            }
+
+        public string Name { get; }
+        public int Interval { get; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BetterTimer timer = new BetterTimer("Timer1", 2000, false);
+            timer.timerRing += BetterTimer_TimerRing;
+            timer.Start();
+            BetterTimer timer2 = new BetterTimer("Timer2", 1000, false);
+            timer.timerRing += BetterTimer_TimerRing;
+            
+
+
+            Task t1 = new Task(()=> timer.Start());
+            Task t2 = new Task(() => timer2.Start());
+            
+
+
+            Console.ReadLine();
+            timer.Stop();
+            timer2.Stop();
+            Console.ReadLine();
+        }
+
+        public static void BetterTimer_TimerRing(BetterTimer timer, BetterTimerEventArgs arg)
+        {
+            Console.WriteLine($"Timer {timer.Name} has gone off for the {arg.HowManyTimesHasItfired} time since {arg.WhenStart}.");
+        }
+    }
+}
