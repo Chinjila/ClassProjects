60bb500652f3579c59595a66695950756dc0fec2
victor <junk@objectmage.com>
2022-11-02 09:43:03 -0700

    Show that events from multiple objects can be handled by the same handler and one event can have multiple handlers. Many to Many.

--- TimerEventSample/Program.cs
+++ TimerEventSample/Program.cs
@@ -4,23 +4,31 @@ using System.Timers;
 System.Timers.Timer timer = new System.Timers.Timer(2000);
 System.Timers.Timer timer2 = new System.Timers.Timer(5000);
 System.Timers.Timer timer3 = new System.Timers.Timer(3000);
+System.Timers.Timer timer4 = new System.Timers.Timer(3000);
 
 
 timer.Elapsed += Timer_Elapsed;
+timer.Elapsed += blah;
 timer.Enabled = true;
 timer2.Elapsed += Timer_Elapsed;
 timer2.Enabled = true;
 timer3.Elapsed += Timer_Elapsed;
 timer3.Enabled = true;
-
+timer4.Elapsed += blah;
+timer4.Enabled = true;
 Console.ReadLine();
-
+void blah(object? sender, ElapsedEventArgs e) {
+    Console.WriteLine($"Blah handle an event generate at {e.SignalTime}");
+}
 void Timer_Elapsed(object? sender, ElapsedEventArgs e)
 {
+    
     if (sender is System.Timers.Timer)
     { 
-        System.Timers.Timer theOneWhoRaisedTheEvent = sender as System.Timers.Timer; 
+        System.Timers.Timer? theOneWhoRaisedTheEvent = sender as System.Timers.Timer;
+        
+        Console.WriteLine($"Timer Event Received from the timer with {theOneWhoRaisedTheEvent?.Interval} interval,signaled at {e.SignalTime}");
     }
 
- Console.WriteLine("Timer Event Received");
+
 }
