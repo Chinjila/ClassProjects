36ed09533947d586ea83d07f47781d39a2ee9dd3
victor <junk@objectmage.com>
2022-11-02 10:18:48 -0700

    code blocks on synchronous calls when engageTimer method is called.

---
+++ TimerEventSample/BetterTimer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerEventSample
+{
+    public delegate void delTimerRing(BetterTimer timer, BetterTimerEventArg arg);
+    public class BetterTimerEventArg {
+        public DateTime WhenStart;
+        public int HowManyTimesHasItFired;
+    }
+    public class BetterTimer
+    {
+        public string Name { get; }
+        public int Interval { get; }
+        DateTime whenStart;
+        int howManyTimesHasItFired=0;
+
+        bool runTimer = false;
+        public event delTimerRing TimerRing; //this is the event
+        public BetterTimer(string name, int interval, bool startNow)
+        {
+            Name = name;
+            Interval = interval;
+            if (startNow)
+            {
+                runTimer = true;
+                EngageTimer();
+            }
+        }
+        private void EngageTimer()
+        {
+            whenStart = DateTime.Now;
+            while (runTimer) {
+                Task.Delay(Interval);
+                BetterTimerEventArg betterTimerEventArg = new()
+                { HowManyTimesHasItFired= howManyTimesHasItFired,
+                  WhenStart = whenStart
+                };
+                TimerRing?.Invoke(this, betterTimerEventArg);
+                howManyTimesHasItFired++;
+            }
+        }
+        public void Start() {
+            if (!runTimer) { 
+                runTimer = true;
+                EngageTimer();
+            }
+        }
+
+        public void Stop()
+        {
+            if (runTimer)
+            {
+                runTimer = false;
+            }
+        }
+    }
+
+
+}
--- TimerEventSample/Program.cs
+++ TimerEventSample/Program.cs
@@ -1,34 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Timers;
+using TimerEventSample;
 
-System.Timers.Timer timer = new System.Timers.Timer(2000);
-System.Timers.Timer timer2 = new System.Timers.Timer(5000);
-System.Timers.Timer timer3 = new System.Timers.Timer(3000);
-System.Timers.Timer timer4 = new System.Timers.Timer(3000);
+//TestSystemTimer();
 
+BetterTimer betterTimer = new BetterTimer("Timer1", 3000, true);
+betterTimer.TimerRing += BetterTimer_TimerRing;
 
-timer.Elapsed += Timer_Elapsed;
-timer.Elapsed += blah;
-timer.Enabled = true;
-timer2.Elapsed += Timer_Elapsed;
-timer2.Enabled = true;
-timer3.Elapsed += Timer_Elapsed;
-timer3.Enabled = true;
-timer4.Elapsed += blah;
-timer4.Enabled = true;
+void BetterTimer_TimerRing(BetterTimer timer, BetterTimerEventArg arg)
+{
+    Console.WriteLine($"{timer.Name} just fired for the {arg.HowManyTimesHasItFired} time since {arg.WhenStart}");
+}
+
 Console.ReadLine();
-void blah(object? sender, ElapsedEventArgs e) {
+void blah(object? sender, ElapsedEventArgs e)
+{
     Console.WriteLine($"Blah handle an event generate at {e.SignalTime}");
 }
 void Timer_Elapsed(object? sender, ElapsedEventArgs e)
 {
-    
+
     if (sender is System.Timers.Timer)
-    { 
+    {
         System.Timers.Timer? theOneWhoRaisedTheEvent = sender as System.Timers.Timer;
-        
+
         Console.WriteLine($"Timer Event Received from the timer with {theOneWhoRaisedTheEvent?.Interval} interval,signaled at {e.SignalTime}");
     }
 
 
+}
+
+void TestSystemTimer()
+{
+    System.Timers.Timer timer = new System.Timers.Timer(2000);
+    System.Timers.Timer timer2 = new System.Timers.Timer(5000);
+    System.Timers.Timer timer3 = new System.Timers.Timer(3000);
+    System.Timers.Timer timer4 = new System.Timers.Timer(3000);
+
+
+    timer.Elapsed += Timer_Elapsed;
+    timer.Elapsed += blah;
+    timer.Enabled = true;
+    timer2.Elapsed += Timer_Elapsed;
+    timer2.Enabled = true;
+    timer3.Elapsed += Timer_Elapsed;
+    timer3.Enabled = true;
+    timer4.Elapsed += blah;
+    timer4.Enabled = true;
 }
