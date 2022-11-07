717be610a5f9be7ab1a492543bd2005274a19dd6
victor <junk@objectmage.com>
2022-11-07 12:09:31 -0500

    completed brotli compression

--- BrotliSample/Program.cs
+++ BrotliSample/Program.cs
@@ -1,7 +1,32 @@
 ﻿//1. Create a File Stream to store compressed data
+using System.IO.Compression;
 
+FileStream outputFile = new FileStream("Compressed.File",FileMode.Create);
 //2. Create a Brotli Stream with above FileStream as part of constructor
+BrotliStream compressor = new BrotliStream(outputFile, CompressionLevel.SmallestSize);
 
 //3. Use a StreamWriter to write some text in to the Brotli Stream
+StreamWriter writer = new StreamWriter(compressor);
 
+
+for (int i = 0; i < 1000; i++)
+{
+    writer.WriteLine($"Hello World {i}");
+}
+
+compressor.Flush(); // must call the compressor Flush or it produce 0 byte file
 //4. Don't forget to flush and close the stream
+outputFile.Flush();
+outputFile.Close();
+
+//Uncompressed file to compare with the brotli stream
+FileStream outputFile2 = new FileStream("Uncompressed.File", FileMode.Create);
+StreamWriter writer2 = new StreamWriter(outputFile2);
+
+
+for (int i = 0; i < 1000; i++)
+{
+    writer2.WriteLine($"Hello World {i}");
+}
+outputFile2.Flush();
+outputFile2.Close();
