74aea2e647fd08887efaee5e858e05e83e56e2e3
kinkeadian@outlook.com <kinkeadian@outlook.com>
2022-11-07 08:37:52 -0800

    json

---
+++ JsonExercise/Product.cs
@@ -0,0 +1,10 @@
+﻿internal class Product
+{
+    public string Name { get; set; }
+    public DateTime Expiry { get; set; }
+    public string[] Sizes { get; set; }
+    public Product()
+    {
+        
+    }
+}
---
+++ JsonExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+Product product = new Product();
+product.Name = "Apple";
+product.Expiry = new DateTime(2008, 12, 28);
+product.Sizes = new string[] { "Small" };
+
+SerializeJson(product);
+
+string json = File.ReadAllText("product.json");
+Product p = JsonConvert.DeserializeObject<Product>(json);
+Console.WriteLine(p.Name);
+
+static void SerializeJson(Product product)
+{
+    string json = JsonConvert.SerializeObject(product);
+    Console.WriteLine(json);
+
+    File.WriteAllText(@"product.json", JsonConvert.SerializeObject(product));
+    using (StreamWriter file = File.CreateText(@"product2.json"))
+    {
+        JsonSerializer serializer = new JsonSerializer();
+        serializer.Serialize(file, product);
+    }
+}
