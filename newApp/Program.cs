e2b366f259d634e92a01f5a16d88b40180e144a4
Jarigo <johnarigo96706@gmail.com>
2022-11-07 09:02:46 -0800

    test

--- newApp/Program.cs
+++ newApp/Program.cs
@@ -3,6 +3,9 @@ namespace project
 {
     class Program
     {
+
+        FileStream newData = new FileStream("Compressed.File", FileMode.Create);
+        
         static void Main(string[] args)
         {
             foreach (var arg in args)
