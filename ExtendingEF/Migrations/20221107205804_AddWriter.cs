70ef93e358b88a67e5230a237ac0447831d446ab
victor <junk@objectmage.com>
2022-11-07 18:20:19 -0500

    Added ExtendingEF Project, learn about linq and EF context and entities

---
+++ ExtendingEF/Migrations/20221107205804_AddWriter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExtendingEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWriter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    WriterID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsVerified = table.Column<bool>(type: "INTEGER", nullable: false),
+                    JoinedOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.WriterID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogWriter",
+                columns: table => new
+                {
+                    BlogsBlogId = table.Column<int>(type: "INTEGER", nullable: false),
+                    WritersWriterID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogWriter", x => new { x.BlogsBlogId, x.WritersWriterID });
+                    table.ForeignKey(
+                        name: "FK_BlogWriter_Blogs_BlogsBlogId",
+                        column: x => x.BlogsBlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogWriter_Writers_WritersWriterID",
+                        column: x => x.WritersWriterID,
+                        principalTable: "Writers",
+                        principalColumn: "WriterID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogWriter_WritersWriterID",
+                table: "BlogWriter",
+                column: "WritersWriterID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogWriter");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
---
+++ ExtendingEF/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var context = new BloggingContext();
+
+var aNewBlog = new Blog { Url = "https://www.chinzilla.com/blogs" };//create a new instance of blog
+
+var aNewPost = new Post { Content = "Hello World", Title = "A new Blog" };//create a new instance of post
+
+aNewBlog.Posts.Add(aNewPost);//associate post to the blog
+
+var writer = new Writer { JoinedOn = DateTime.Now, IsVerified = true, Name = "bob" };//create new instance of writer
+
+aNewBlog.Writers.Add(writer);//associate writer bob with the new blog
+
+context.Blogs.Add(aNewBlog);//adding blob pulls in all related entities: writer and post
+//use AddRange to add array of Writers to the context.Writers
+context.Writers.AddRange(
+    new Writer { JoinedOn = DateTime.Now, IsVerified = true, Name = "bob2" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = false, Name = "bob3" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = true, Name = "bob4" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = false, Name = "bob5" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = false, Name = "bob7" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = false, Name = "bob6" },
+    new Writer { JoinedOn = DateTime.Now, IsVerified = true, Name = "bob" });
+
+context.SaveChanges(); //save all pending changes to the database
+
+
+var query = context.Writers.Where(w => w.Name.StartsWith("bob")); //find all writers whose name starts with bob
+
+var query2 = query.Where(w => w.IsVerified).Select(w=>new { w.Name,w.JoinedOn }); ; 
+//the query is not resolved yet, so we can append additional criteria and refine our result
+
+query2.ToList().ForEach(w => Console.WriteLine($"{w.Name} joined on {w.JoinedOn}"));//ToList will trigger a sql command execution behind the scene
+//ForEach is a convenient extension method to print each writer's name to the console.
+
+Console.ReadLine();
+//.Database.SqlQuery<Writer>($"Select * from writers where witerID=1");
+
+// How to group all writers by their name?
+var query3 = (from w in context.Writers
+             group w by w.Name into nameGroup
+             where nameGroup.Count()>3 && nameGroup.Key.Contains("bob")
+             select new { Name = nameGroup.Key, CountOfWriter = nameGroup.Count() }).Take(2);
+
+query3.ToList().ForEach(g => Console.WriteLine(
+    $"there are {g.CountOfWriter} {g.Name} in database."
+    ));
