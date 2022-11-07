82c91ccacd77893135e68d41de682f1449410b70
victor <junk@objectmage.com>
2022-11-07 12:37:58 -0500

    Flushes the writer as last few lines of text didn't make to the output file

--- BrotliSample/Program.cs
+++ BrotliSample/Program.cs
@@ -8,11 +8,14 @@ BrotliStream compressor = new BrotliStream(outputFile, CompressionLevel.Smallest
 //3. Use a StreamWriter to write some text in to the Brotli Stream
 StreamWriter writer = new StreamWriter(compressor);
 
+//writer.AutoFlush = true;
 
 for (int i = 0; i < 1000; i++)
 {
     writer.WriteLine($"Hello World {i}");
 }
+
+writer.Flush();//manually flush the buffer
 
 compressor.Flush(); // must call the compressor Flush or it produce 0 byte file
 //4. Don't forget to flush and close the stream
@@ -22,11 +25,15 @@ outputFile.Close();
 //Uncompressed file to compare with the brotli stream
 FileStream outputFile2 = new FileStream("Uncompressed.File", FileMode.Create);
 StreamWriter writer2 = new StreamWriter(outputFile2);
-
+//writer2.AutoFlush=true;
 
 for (int i = 0; i < 1000; i++)
 {
     writer2.WriteLine($"Hello World {i}");
 }
+writer2.Flush();
+
 outputFile2.Flush();
+
 outputFile2.Close();
+Console.ReadLine();
