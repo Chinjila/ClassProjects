8e1be44032a8ffcdf4e2457dd71df757e76c4256
victor <junk@objectmage.com>
2022-11-07 11:35:35 -0500

    Added Brotli Sample Starter Code

---
+++ BrotliSample/Program.cs
@@ -0,0 +1,7 @@
+﻿//1. Create a File Stream to store compressed data
+
+//2. Create a Brotli Stream with above FileStream as part of constructor
+
+//3. Use a StreamWriter to write some text in to the Brotli Stream
+
+//4. Don't forget to flush and close the stream
---
+++ EFHelloWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+using var db = new BloggingContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+
+
+//db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+
+//var anotherBlog = new Blog { Url = "http://blogs.google.com/adonet" };
+
+//db.Add(anotherBlog);
+
+//db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+foreach (var aBlog in db.Blogs)
+{
+    Console.WriteLine(aBlog.Url);
+    for (int i = 0; i < 10; i++)
+    {
+        Post p = new Post() { Blog = aBlog, Content = $"Post#{i}", Title = $"PostTitle#{i}" };
+        db.Posts.Add(p);
+    }
+    
+}
+
+db.SaveChanges();
+
+var result = db.Posts.Where(p => p.Title == "PostTitle#7");
+
+foreach (var item in result)
+{
+    Console.WriteLine($"{item.PostId},{item.Title},{item.BlogId}");
+}
+
+//// Delete
+//Console.WriteLine("Delete the blog");
+//db.Remove(blog);
+//db.SaveChanges();
