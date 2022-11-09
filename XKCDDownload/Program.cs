c7c1ed551c72a7943ebdbc10c48492957b711f17
victor <junk@objectmage.com>
2022-11-08 21:33:13 -0500

    Add Parallel For loop

--- XKCDDownload/Program.cs
+++ XKCDDownload/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Concurrent;
 using System.Reflection;
 using System.Text.Json;
 
@@ -14,20 +15,24 @@ if (!Directory.Exists(destinationFolder)) {
 }
 List<Task> tasks = new List<Task>();
 List<string>? imgList = new List<string>();
+
+ConcurrentBag<string>? bag = new();
+
 //string url = "https://xkcd.com/1500/info.0.json";
+
 if (File.Exists("list.json"))
-{ 
-    imgList=JsonSerializer.Deserialize<List<string>>(File.Open("list.json",FileMode.Open));
+{
+    imgList = JsonSerializer.Deserialize<List<string>>(File.Open("list.json",FileMode.Open));
 }
 else
 {
-    for (int i = 400; i < 1600; i++)
+    var result = Parallel.For(500, 1000,(i) =>
     {
         try
         {
-            var stream = await client.GetStreamAsync($"https://xkcd.com/{i}/info.0.json");
-            var aComic = await JsonSerializer.DeserializeAsync<Comic>(stream);
-            imgList.Add(aComic!.img);
+            var stream = client.GetStreamAsync($"https://xkcd.com/{i}/info.0.json").Result;
+            var aComic = JsonSerializer.DeserializeAsync<Comic>(stream).Result;
+            bag.Add(aComic.img);
         }
         catch (Exception)
         {
@@ -35,9 +40,14 @@ else
             Console.WriteLine($"https://xkcd.com/{i}/info.0.json download failed");
         }
 
-    }
+    });
+
+    imgList= bag.ToList<string>();
+    Console.WriteLine($"{imgList.Count} files to download." );
     File.WriteAllText("list.json", JsonSerializer.Serialize(imgList));
 }
+
+
 Parallel.ForEach(imgList!,new ParallelOptions { MaxDegreeOfParallelism=8 },(imgUrl,state,index) => {
     var task = client.GetStreamAsync(imgUrl).ContinueWith(
              async (t) => { await t.Result.CopyToAsync(File.Create(destinationFolder + $"\\{index}.png")); Console.WriteLine($"file {index} completed"); }
