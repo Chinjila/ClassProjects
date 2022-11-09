5e77e40adca055d60e97a69a814e50f3c6a2fa83
victor <junk@objectmage.com>
2022-11-08 20:56:55 -0500

    Add parallel download per request

--- XKCDDownload/Program.cs
+++ XKCDDownload/Program.cs
@@ -1,38 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Reflection;
 using System.Text.Json;
 
-HttpClient client = new();
+HttpClient client = new(new HttpClientHandler
+{
+    AllowAutoRedirect = true,
+    MaxAutomaticRedirections = 2
+});
 var destinationFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\xkcd";
 if (!Directory.Exists(destinationFolder)) {
     Directory.CreateDirectory(destinationFolder);   
 }
-
+List<Task> tasks = new List<Task>();
+List<string>? imgList = new List<string>();
 //string url = "https://xkcd.com/1500/info.0.json";
-for (int i = 1000; i < 1200; i++)
-{
-    var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, $"https://xkcd.com/{i}/info.0.json"));
-    if (response.IsSuccessStatusCode) {
-
-        var stream = await client.GetStreamAsync($"https://xkcd.com/{i}/info.0.json");
-
-        var aComic = await JsonSerializer.DeserializeAsync<Comic>(stream);
-
-        var imgResponse = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, aComic?.img));
-        if (imgResponse.IsSuccessStatusCode) {
-
-            var comicStream = await client.GetStreamAsync(aComic?.img);
-            var fileStream = File.Create(destinationFolder + $"\\{i}.png");
-            comicStream.CopyTo(fileStream);
-
-            Console.WriteLine($"Comic {i} downloaded - {fileStream.Length} bytes");
-            fileStream.Close();
-            comicStream.Close();
-        }
-        
-    }
-    
+if (File.Exists("list.json"))
+{ 
+    imgList=JsonSerializer.Deserialize<List<string>>(File.Open("list.json",FileMode.Open));
 }
+else
+{
+    for (int i = 400; i < 1600; i++)
+    {
+        try
+        {
+            var stream = await client.GetStreamAsync($"https://xkcd.com/{i}/info.0.json");
+            var aComic = await JsonSerializer.DeserializeAsync<Comic>(stream);
+            imgList.Add(aComic!.img);
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine($"https://xkcd.com/{i}/info.0.json download failed");
+        }
+
+    }
+    File.WriteAllText("list.json", JsonSerializer.Serialize(imgList));
+}
+Parallel.ForEach(imgList!,new ParallelOptions { MaxDegreeOfParallelism=8 },(imgUrl,state,index) => {
+    var task = client.GetStreamAsync(imgUrl).ContinueWith(
+             async (t) => { await t.Result.CopyToAsync(File.Create(destinationFolder + $"\\{index}.png")); Console.WriteLine($"file {index} completed"); }
+             );
+        tasks.Add(task);
+    }
+
+    );
+Task.WaitAll(tasks.ToArray());
+Console.WriteLine("Done");
 record class Comic(
     string img,
     string title
